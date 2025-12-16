a42550f19b8362200647228ce8a471ed0ad9e5a9
Ахмед <ismailov.ahmed.2004@gmail.com>
2025-12-16 22:23:59 +0300

    Add custom User model and migrations

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import TelegramProfile
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(TelegramProfile)
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    pass
